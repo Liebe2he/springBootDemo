8f4a2aeb6da5679244cd5f98c1feb9cc35f99da8
moyuxi <402575153@qq.com>
2019-11-11 17:46:13 +0800

    新增GetMapping接口

--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,10 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
  * @create 2019-10-30 15:26
  */
 @RestController
+@RequestMapping("api/v1")
 public class HelloWorldController {
 
     @RequestMapping("/hello")
     public String index() {
         return "Hello World";
+    }
+
+    @GetMapping("/get")
+    public String Get() {
+        return "get success!";
     }
 }
